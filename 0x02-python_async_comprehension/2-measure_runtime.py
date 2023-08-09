aca132e5d161465702d669bb5b67a1085c079a47
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-08-09 10:08:39 +0000

    Add files via upload

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Mesures,, .. time"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """async_comprehensiand return"""
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start_time
